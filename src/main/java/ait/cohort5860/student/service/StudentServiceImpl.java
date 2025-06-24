cb590f62d5123313e0bfe56d338a32458ee969bb
Andrey Shemet <an-shem@ukr.net>
2025-06-24 03:19:04 +0200

    Corrected the methods in StudentServiceImpl

--- src/main/java/ait/cohort5860/student/service/StudentServiceImpl.java
+++ src/main/java/ait/cohort5860/student/service/StudentServiceImpl.java
@@ -8,8 +8,6 @@ import ait.cohort5860.student.dto.StudentUpdateDto;
 import ait.cohort5860.student.dto.exceptions.NotFoundException;
 import ait.cohort5860.student.model.Student;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +16,7 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
-       private final StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
     @Override
     public Boolean addStudent(StudentCredentialsDto studentCredentialsDto) {
@@ -43,29 +41,30 @@ public class StudentServiceImpl implements StudentService {
         return studentDto;
     }
 
-//    @Override
-//    public StudentCredentialsDto updateStudentById(Long id, StudentUpdateDto studentUpdateDto) {
-//        Student student = studentRepository.findById(id).orElseThrow(NotFoundException::new);
-//        Student updateStudent = new Student(id,
-//                studentUpdateDto.getName() == null ?
-//                        student.getName() : studentUpdateDto.getName(),
-//                studentUpdateDto.getPassword() == null ?
-//                        student.getPassword() : studentUpdateDto.getPassword());
-//        studentRepository.save(updateStudent);
-//        return new StudentCredentialsDto(id, updateStudent.getName(), updateStudent.getPassword());
-//    }
-
     @Override
     public StudentCredentialsDto updateStudentById(Long id, StudentUpdateDto studentUpdateDto) {
         Student student = studentRepository.findById(id).orElseThrow(NotFoundException::new);
-        if (studentUpdateDto.getName() != null) {
-            student.setName(studentUpdateDto.getName());
-        }
-        if (studentUpdateDto.getPassword() != null) {
-            student.setPassword(studentUpdateDto.getPassword());
-        }
-        return new StudentCredentialsDto(student.getId(), student.getName(), student.getPassword());
+        Student updateStudent = new Student(id,
+                studentUpdateDto.getName() == null ?
+                        student.getName() : studentUpdateDto.getName(),
+                studentUpdateDto.getPassword() == null ?
+                        student.getPassword() : studentUpdateDto.getPassword());
+        studentRepository.save(updateStudent);
+        return new StudentCredentialsDto(id, updateStudent.getName(), updateStudent.getPassword());
     }
+
+// ----------------------- Вариант учителя ( подходит для МАР но не для базы данных, т. к. не сохраняет)---------------
+//    @Override
+//    public StudentCredentialsDto updateStudentById(Long id, StudentUpdateDto studentUpdateDto) {
+//        Student student = studentRepository.findById(id).orElseThrow(NotFoundException::new);
+//        if (studentUpdateDto.getName() != null) {
+//            student.setName(studentUpdateDto.getName());
+//        }
+//        if (studentUpdateDto.getPassword() != null) {
+//            student.setPassword(studentUpdateDto.getPassword());
+//        }
+//        return new StudentCredentialsDto(student.getId(), student.getName(), student.getPassword());
+//    }
 
     @Override
     public Boolean addScore(Long id, ScoreDto scoreDto) {
@@ -77,7 +76,7 @@ public class StudentServiceImpl implements StudentService {
     public List<StudentDto> findStudentsByName(String name) {
         List<Student> students = studentRepository.findAll();
         List<StudentDto> studentDtoList = students.stream()
-                .filter(student -> student.getName().toLowerCase().equals(name))
+                .filter(student -> name.equalsIgnoreCase(student.getName()))
                 .map(student -> new StudentDto(student.getId(), student.getName(), student.getScores()))
                 .toList();
         return studentDtoList;
@@ -85,21 +84,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Long countStudentsByName(Set<String> names) {
-        List<Student> students = studentRepository.findAll();
-        Long quantity = students.stream()
+        return studentRepository.findAll().stream()
                 .filter(student -> names.contains(student.getName()))
                 .count();
-        return quantity;
+
     }
 
     @Override
     public List<StudentDto> findStudentsByExamName(String examName, Integer minScore) {
-        List<Student> students = studentRepository.findAll();
-        List<StudentDto> studentDtoList = students.stream()
-                .filter(student -> student.getScores().containsKey(examName)
-                        && student.getScores().get(examName) >= minScore)
-                .map(student -> new StudentDto(student.getId(), student.getName(), student.getScores()))
+        return studentRepository.findAll().stream()
+                .filter(s -> s.getScores().containsKey(examName)
+                        && s.getScores().get(examName) > minScore)
+                .map(s -> new StudentDto(s.getId(), s.getName(), s.getScores()))
                 .toList();
-        return studentDtoList;
+
     }
 }
